56b4c84d6d9448408a61ee1a7cf70478a242d4f1
Vannghianp09 <vannghiaqng.2004@gmail.com>
2026-03-27 15:25:32 +0700

    fix_Lv5_6

--- FE_ITCEP/src/App.tsx
+++ FE_ITCEP/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
