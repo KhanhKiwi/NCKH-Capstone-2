e20dcb2fc08a0acd9b43cad31d44ecb4ac509a4f
doanhphan2110 <doanhphan2110@gmail.com>
2026-04-01 01:18:20 +0700

    nhạc toàn trang web

--- FE_ITCEP/src/App.tsx
+++ FE_ITCEP/src/App.tsx
@@ -1,10 +1,14 @@
+
 import { RouterProvider } from "react-router";
 import { router } from "./routes";
 import "./App.css";
+import BackgroundMusic from "./components/BackgroundMusic/BackgroundMusic";
+
 
 function App() {
   return (
     <>
+      <BackgroundMusic />
       <RouterProvider router={router} />
     </>
   );
