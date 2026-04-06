e2fb6bdc1d07db24e042a15444504d98428e463d
doanhphan2110 <doanhphan2110@gmail.com>
2026-04-07 00:40:26 +0700

    xóa nhạc trong trang admin

--- FE_ITCEP/src/routes/RootLayout.tsx
+++ FE_ITCEP/src/routes/RootLayout.tsx
@@ -1,10 +1,16 @@
-import { Outlet } from 'react-router'
+import { Outlet, useLocation } from 'react-router'
 import ScrollToTop from '../components/ScrollToTop'
+import BackgroundMusic from '../components/BackgroundMusic/BackgroundMusic'
 
 export default function RootLayout() {
+  const loc = useLocation()
+  const isAdmin = loc.pathname.startsWith('/admin')
+
   return (
     <>
       <ScrollToTop />
+      {/* render background music on non-admin pages only */}
+      {!isAdmin && <BackgroundMusic />}
       <Outlet />
     </>
   )
