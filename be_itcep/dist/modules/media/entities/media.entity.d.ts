14d9932825dd302121c79f0e5803e224857c9992
levannghia4-Danang <141853819+chutupro@users.noreply.github.com>
2026-05-10 14:03:51 +0700

    Remove dist from git tracking (should be ignored)

--- be_itcep/dist/modules/media/entities/media.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { CraftVillage } from '../../villages/entities/craft-village.entity';
-export declare class Media {
-    media_id: number;
-    village: CraftVillage;
-    url: string;
-    created_at: Date;
-    updated_at: Date;
-    deleted_at: Date;
-}
